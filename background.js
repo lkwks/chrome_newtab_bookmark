c3c50d6f4fe2276540faef42393463eb53a2d374
Kangwook Lee <lkwks@naver.com>
2023-01-23 18:37:33 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,41 +1,436 @@
-chrome.bookmarks.getTree(async (b)=>
+document.body.addEventListener("click", (e)=>
 {
-    var bookmark = b[0].children[0].children, img_dict = {};
-    for (var e of bookmark)
+    console.log(e.target);
+    if (e.target.nodeName === "SPAN" && e.target.parentNode.classList.contains("weather_info"))
     {
-        var url = "";
-        try
-        {
-            url = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://${ (new URL(e.url)).hostname}&size=64`;
-            // 구글이 소셜앱에서 favicon 구해오는 히든 api를 사용. 
-        }
-        catch(error) // 폴더 계층 구조 구현은 못할 건 없는데 워낙 귀찮기도 하고 별로 쓸 것 같지도 않고... 
-        {
-            continue;
-        }
+        var target = document.querySelector("iframe");
+        if (target.classList.contains("widen") === false)
+            target.classList.add("widen");
+        else
+            target.classList.remove("widen");
+    }
+
+    if (e.target.nodeName === "DIV" && e.target.id !== "")
+    {
+        if (e.target.querySelector("a"))
+            window.location.href = e.target.querySelector("a").href;
+        else
+            main.move_folder(e.target.id);
+    }
+
+    if (e.target.nodeName === "P" && e.target.classList.contains("arrow_box"))
+    {
+        if (e.target.parentNode.querySelector("a"))
+            window.location.href = e.target.parentNode.querySelector("a").href;
+        else
+            main.move_folder(e.target.parentNode.id);
+    }
+
+    if ((e.target.nodeName === "DIV" && e.target.childNodes.length > 0 && e.target.childNodes[0].nodeName === "FONT") || e.target.nodeName === "FONT")
+        mod_box.new_mod_box();
+
+    if (e.target.id === "overlay" || (e.target.nodeName === "BUTTON" && e.target.classList.contains("cancel")))
+        mod_box.close_mod_box();
+
+    if (e.target === document.querySelector("button.confirm"))
+        mod_box.save_and_close();
+
+    if (e.target.nodeName === "P" && e.target.classList.contains("mod_button"))
+        chrome.bookmarks.get(e.target.parentNode.id, (e) => { mod_box.mod_box(e[0]); });
+
+    if (e.target.nodeName === "BUTTON" && e.target.classList.contains("delete"))
+        mod_box.delete();
+
+    if (e.target.nodeName === "SPAN" && e.target.classList.contains("new_bookmark"))
+        mod_box.show_new_bookmark();
     
-        img_dict[url] = new Image();
-        img_dict[url].src = url;
-        img_dict[url].id = e.url;
-        img_dict[url].alt = e.title.substring(0,2);
-        img_dict[url].onload = (e) => {
-            if (e.target.width === 16)
-                document.querySelector(`a[href='${e.target.id}']`).innerHTML = `<button>${e.target.alt}</button>`;
+    if (e.target.nodeName === "SPAN" && e.target.classList.contains("new_folder"))
+        mod_box.show_new_folder();
+
+    if (e.target.nodeName === "BUTTON" && e.target.classList.contains("folder"))
+        main.move_folder(e.target.parentNode.id);
+
+    if (e.target.nodeName === "SPAN" && e.target.parentNode.classList.contains("folder_list"))
+        main.move_folder(e.target.id);
+});
+
+async function get_folder(id)
+{
+    return new Promise((resolve, reject) => {
+        chrome.bookmarks.get(id, (b) => {
+            resolve(b[0]);
+        })
+    });    
+}
+
+class Main{
+    constructor($target)
+    {
+        this.plus_obj = new Cell();
+        this.plus_obj.put_innerHTML("<div><font style='font-size:40pt;font-weight:100;'>+</font><br></div>")
+        this.$target = $target;
+        this.folder_list_obj = document.querySelector("div.folder_list");
+        this.weather_info_obj = document.querySelector("div.weather_info");
+        this.cells = {};
+        this.img_dict = {};
+        this.folder_stack = [];
+        this.folder_id = "";
+        this.move_folder("1");
+    }
+
+    put(cell, id)
+    {
+        this.cells[id] = cell;
+        this.$target.appendChild(cell.obj);
+    }
+
+    insertBefore(obj1, obj2)
+    {
+        this.$target.insertBefore(obj1, obj2);
+    }
+
+    clear_main()
+    {
+        this.cells = {};
+        this.img_dict = {};
+        this.$target.innerHTML = "";
+        if (this.folder_id !== "1")
+        {
+            if (this.weather_info_obj.classList.contains("hide") === false)
+                this.weather_info_obj.classList.add("hide");
+            this.folder_list_obj.classList.remove("hide");
+        }
+        else
+        {
+            if (this.folder_list_obj.classList.contains("hide") === false)
+                this.folder_list_obj.classList.add("hide");
+            this.weather_info_obj.classList.remove("hide");
+            var now = new Date();
+            this.weather_info_obj.querySelector("span").innerText = `${now.getHours()}:${now.getMinutes()}`;
+        }
+    }
+
+    async print_folder_list()
+    {
+        var folder_stack = [], id = this.folder_id, cnt = 10;
+        while (cnt)
+        {
+            var b = await get_folder(id);
+            folder_stack.push(b);
+            if (b.id !== "1")
+                id = b.parentId;
+            else break;
+            cnt--;
         }
 
-        var div = document.createElement("div");
-        div.classList.add("cell");
-        var title = e.title.length > 10 ? e.title.substring(0,7) + "..." : e.title;
-        div.innerHTML = `<div><a href="${e.url}"><img src="${url}"></a><br>${title}<p class="arrow_box">${e.title}</p></div>`;
-        div.querySelector("div").addEventListener("click", (e)=>{window.location.href = e.target.querySelector("a").href});
-        document.querySelector("main").appendChild(div);
+        this.folder_list_obj.innerHTML = (folder_stack.length === 1)  ? "" : [...folder_stack].reverse().map((e)=>{return `<span id="${e.id}">${e.title}</span>`}).join("&nbsp;>&nbsp;") + "&nbsp;>";
     }
 
-    for (var i = 0; i < (5 - bookmark.length % 5) % 5; i++)
+    move_folder(id)
     {
-        var div = document.createElement("div");
-        div.classList.add("cell");
-        document.querySelector("main").appendChild(div);
+        this.folder_id = id;
+        this.print_folder_list();
+        this.clear_main();
+        chrome.bookmarks.getChildren(id, async (b)=>
+        {
+            for (var e of b)
+            {
+                var cell = new Cell();
+                var icon  = ('url' in e) ? new Icon(e) : new FolderIcon(e);
+                cell.put_innerHTML(await icon.get_innerHTML());
+                this.put(cell, icon.id);
+            }
+        
+            this.put(this.plus_obj, "plus");
+        
+            for (var i = 0; i < 4; i++)
+                this.put(new Cell(), "blank");
+        });        
+    }
+}
+
+class Cell{
+    constructor()
+    {        
+        this.obj = document.createElement("div");
+        this.obj.classList.add("cell");
     }
 
+    put_innerHTML(innerHTML)
+    {
+        this.obj.innerHTML = innerHTML;
+    }
+}
+
+class Icon {
+    constructor(e)
+    {
+        var url = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://${ (new URL(e.url)).hostname}&size=64`;
+        // 구글이 소셜앱에서 favicon 구해오는 히든 api를 사용. 
+        main.img_dict[url] = new Image();
+        main.img_dict[url].src = url;
+        main.img_dict[url].id = e.url;
+        main.img_dict[url].alt = e.title.substring(0,2);
+        main.img_dict[url].onload = (e) => {
+            if (e.target.width === 16)
+                document.querySelector(`a[href='${e.target.id}']`).innerHTML = `<button class="icon">${e.target.alt}</button>`;
+        }
+
+        this.id = e.id;
+        var title = e.title.length > 10 ? e.title.substring(0,7) + "..." : e.title;
+        this.innerHTML = `<div id="${e.id}" draggable="true"><p class="mod_button">=</p><a href="${e.url}" draggable="false"><img src="${url}" draggable="false"></a><br>${title}<p class="arrow_box">${e.title}</p></div>`;
+    }
+
+    async get_innerHTML()
+    {
+        return this.innerHTML;
+    }
+}
+
+class FolderIcon {
+    constructor(e)
+    {
+        this.id = e.id;
+        this.title = e.title;
+        this.innerHTML = "";
+    }
+
+    async get_innerHTML()
+    {
+        return new Promise((resolve, reject) => {
+            chrome.bookmarks.getChildren(this.id, (b)=>{
+                var numbers = ` (${b.length})`;
+                var title = (this.title + numbers).length > 10 ? this.title.substring(0,7 - numbers.length) + "..." + numbers: this.title + numbers;
+                resolve(`<div id="${this.id}" draggable="true"><p class="mod_button">=</p><button class="folder_deco"></button><button class="folder"></button><span style="padding:12px;">&nbsp;</span><br>${title}<p class="arrow_box">${this.title}</p></div>`);
+            });
+        })
+    }
+}
+
+
+
+var now_dragging = null, start_pos = null, folder_selected = null, dragleave = null;
+
+document.body.addEventListener("dragstart", (e) =>{
+    if (e.target.nodeName === "DIV" && "id" in e.target && e.target.id !== "")
+    {
+        main.$target.childNodes.forEach((elem, i) => {
+            if (elem === e.target.parentNode)
+            {
+                start_pos = i;
+                now_dragging = i;
+            }
+        })
+        e.target.style.opacity = 0.01;
+        e.target.classList.add("not_hover");
+    }
 });
+
+document.body.addEventListener("dragover", (e)=> {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+});
+
+
+document.body.addEventListener("dragenter", (e) => {
+    if (e.target.nodeName === "DIV" && e.target.id !== "")
+    {
+        if (folder_selected) folder_selected.classList.remove("folder_selected");
+        if (e.target.querySelector("a"))
+        {
+            var temp = null;
+            main.$target.childNodes.forEach((elem, i) => {
+                if (elem === e.target.parentNode)
+                    temp = i;
+            });
+            if (now_dragging === temp) return;
+            else
+            {
+                var temp2 = null;
+                if (now_dragging < temp)
+                    temp2 = main.$target.replaceChild(main.$target.childNodes[now_dragging], main.$target.childNodes[temp+1]);
+                else
+                    temp2 = main.$target.replaceChild(main.$target.childNodes[now_dragging], main.$target.childNodes[temp]);
+                main.$target.insertBefore(temp2, main.$target.childNodes[temp+1]);
+                now_dragging = temp;
+            }
+            folder_selected = null;
+        }
+        else
+        {
+            folder_selected = e.target;
+            if (e.target.classList.contains("folder_selected") === false) e.target.classList.add("folder_selected");
+        }
+    }
+
+    if (e.target.nodeName === "SPAN" && e.target.parentNode.classList.contains("folder_list"))
+    {
+        if (folder_selected) folder_selected.classList.remove("folder_selected");        
+        folder_selected = e.target;
+        if (e.target.classList.contains("folder_selected") === false) e.target.classList.add("folder_selected");
+    }
+});
+
+document.body.addEventListener("dragleave", (e) => { 
+    if (e.target.classList.contains("folder_selected")) dragleave = e.target;
+});
+
+document.body.addEventListener("dragend", (e) => {
+    e.target.style.opacity = 1;
+    if (folder_selected && now_dragging !== null)
+    {
+        folder_selected.classList.remove("folder_selected");
+        if (main.folder_id !== folder_selected.id && e.target.id !== folder_selected.id && folder_selected !== dragleave)
+        {
+            console.log(dragleave);
+            chrome.bookmarks.move(e.target.id, {parentId: folder_selected.id});
+            main.$target.removeChild(e.target.parentNode);
+        }
+        else
+        {
+            now_dragging = start_pos < now_dragging ? now_dragging +1 : now_dragging;
+            chrome.bookmarks.move(e.target.id, {parentId: main.folder_id, index:now_dragging});
+        }
+        folder_selected = null;
+        dragleave = null;
+    }
+    else if (now_dragging !== null)
+    {
+        now_dragging = start_pos < now_dragging ? now_dragging +1 : now_dragging;
+        chrome.bookmarks.move(e.target.id, {parentId: main.folder_id, index:now_dragging});
+    }
+    now_dragging = null;
+    start_pos = null;
+});
+
+document.body.addEventListener("keydown", (e) => {
+    if (e.target.nodeName === "INPUT" && e.key === "Enter")
+        mod_box.save_and_close();
+});
+
+
+class ModBox
+{
+    constructor($target)
+    {
+        this.$target = $target;
+        this.overlay_obj = document.getElementById("overlay");
+        this.name_obj = $target.querySelector("input.name");
+        this.url_obj = $target.querySelector("input.url");
+        this.del_obj = $target.querySelector("button.delete");
+        this.new_head_obj = $target.querySelector("h2.new");
+        this.mod_head_obj = $target.querySelector("h2.mod");
+        this.url_div_obj = $target.querySelector("div.url");
+        this.new_bookmark_obj = $target.querySelector("span.new_bookmark");
+        this.new_folder_obj = $target.querySelector("span.new_folder");
+        this.close_mod_box();
+        this.elem = null;
+    }
+
+    save_and_close()
+    {
+        if (this.name_obj.value === "" || (this.url_obj.value !== null && this.url_obj.value === "https://"))
+        {
+            alert("Input Error!");
+            return;
+        }
+
+        this.close_mod_box();
+        if (this.elem === null)
+        {
+            if (this.url_obj.value === null)
+                chrome.bookmarks.create({'parentId':  main.folder_id, 'title': this.name_obj.value, 'folder': true});
+            else
+                chrome.bookmarks.create({'parentId':  main.folder_id, 'title': this.name_obj.value, 'url': this.url_obj.value});
+            chrome.bookmarks.getChildren(main.folder_id, async (bookmarks) => {
+                var b = bookmarks[bookmarks.length-1];
+                var cell = new Cell();
+                var icon = ('url' in b) ? new Icon(b) : new FolderIcon(b);
+                cell.put_innerHTML(await icon.get_innerHTML());
+                main.cells[b.id] = cell;
+                main.insertBefore(cell.obj, main.plus_obj.obj);
+            });
+        }
+        else
+        {
+            chrome.bookmarks.update(this.elem.id, {'title': this.name_obj.value, 'url': this.url_obj.value});
+            chrome.bookmarks.get(this.elem.id, async (e) => { 
+                var icon = "url" in e[0] ? new Icon(e[0]) : new FolderIcon(e[0]);
+                main.cells[e[0].id].put_innerHTML(await icon.get_innerHTML());
+            });
+        }
+    }
+
+    delete()
+    {
+        this.close_mod_box();
+        chrome.bookmarks.remove(this.elem.id);
+        main.$target.removeChild(main.cells[this.elem.id].obj);
+    }
+
+    close_mod_box()
+    {
+        this.$target.classList.add("hide");
+        this.overlay_obj.classList.add("hide");
+    }
+
+    new_mod_box()
+    {
+        this.show_mod_box();
+        this.show_new_bookmark();
+        if (this.del_obj.classList.contains("hide") === false) this.del_obj.classList.add("hide");
+        if (this.mod_head_obj.classList.contains("hide") === false) this.mod_head_obj.classList.add("hide");
+        this.new_head_obj.classList.remove("hide");
+        this.name_obj.value = "";
+        this.url_obj.value = "https://";
+        this.elem = null;
+    }
+
+    show_new_bookmark()
+    {
+        if (this.new_bookmark_obj.classList.contains("clicked") === false) this.new_bookmark_obj.classList.add("clicked");
+        this.url_div_obj.classList.remove("visibility_hide");
+        this.new_folder_obj.classList.remove("clicked");
+        this.url_obj.value = "https://";
+    }
+
+    show_new_folder()
+    {
+        if (this.new_folder_obj.classList.contains("clicked") === false) this.new_folder_obj.classList.add("clicked");
+        if (this.url_div_obj.classList.contains("visibility_hide") === false) this.url_div_obj.classList.add("visibility_hide");
+        this.new_bookmark_obj.classList.remove("clicked");
+        this.url_obj.value = null;
+    }
+
+    mod_box(elem)
+    {
+        this.show_mod_box();
+        if (this.new_head_obj.classList.contains("hide") === false) this.new_head_obj.classList.add("hide");
+        this.mod_head_obj.classList.remove("hide");
+        this.del_obj.classList.remove("hide");
+        this.name_obj.value = elem.title;
+        if ("url" in elem)
+        {
+            this.url_div_obj.classList.remove("visibility_hide");
+            this.url_obj.value = elem.url;
+        }
+        else
+        {
+            if (this.url_div_obj.classList.contains("visibility_hide") === false) this.url_div_obj.classList.add("visibility_hide");
+            this.url_obj.value = null;
+        }
+        this.elem = elem;
+    }
+
+    show_mod_box()
+    {
+        this.$target.classList.remove("hide");
+        this.overlay_obj.classList.remove("hide");
+    }
+}
+
+var main = new Main(document.querySelector("main"));
+var mod_box = new ModBox(document.querySelector("div.mod_box"));
+
+
+
